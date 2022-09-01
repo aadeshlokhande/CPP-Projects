e68f2861ce79f7e348771f204ebd96d72412dc5c
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-01 17:29:55 +0530

    01-09-2022

---
+++ CPP class/001syntax.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello aadesh how are you"<< endl;
+    return 0;
+}
+
+// +++++++++++++++++++++++++
+// #include - preprocessing command
+// iostream - library 
+// int  - return type 
+// main() - function 
+// {} - scope 
+// cout - object
+// << -  right shift operator
+// "Hello world" - string 
+// ; - full stop
+// return - keyword
+// 0 - value
+
+
+// ================================
+// comments - 
+// printing hello world
+/* ----------- */
+/* this 
+is 
+my
+multiline
+comment
+*/
+
+
+// whitespace - blankspace
+
+// tokens - smallest element
+// cout
+//  <<
+//   "Hello world\n"
+//   ;
+
+// keywords - reserve words
+// alignas	decltype	namespace	struct
+// alignof	default	new	switch
+// and	delete	noexcept	template
+// and_eq	do	not	this
+// asm	double	not_eq	thread_local
+// auto	dynamic_cast	nullptr	throw
+// bitand	else	operator	true
+// bitor	enum	or	try
+// bool	explicit	or_eq	typedef
+// break	export	private	typeid
+// case	extern	protected	typename
+// catch	false	public	union
+// char	float	register	unsigned
+// char16_t	for	reinterpret_cast	using
+// char32_t	friend	return	virtual
+// class	goto	short	void
+// compl	if	signed	volatile
+// const	inline	sizeof	wchar_t
+// constexpr	int	static	while
+// const_cast	long	static_assert	xor
+// continue	mutable	static_cast	xor_eq
+
+
+// escape char
+// \n - new line 
+// \t - horizontal tab 
+
+
+// endl 
