a1bd675e92feeabf89cc6f7d0c9d8c85ce3d91bc
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-02 17:37:17 +0530

    02-09-2022

---
+++ CPP class/002inputOutput.cpp
@@ -0,0 +1,17 @@
+// output 
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "hello";
+}
+
+// input
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a number = ";
+    cin >> a;
+}
