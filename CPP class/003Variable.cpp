b1f35fa4bbe65e4d40a3957f7042ecf08487cd32
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-03 18:04:32 +0530

    03-09-2022

---
+++ CPP class/003Variable.cpp
@@ -0,0 +1,83 @@
+// variable - cantainer 
+// vari able
+// a = 10 
+// a = 20 
+// a = 30 
+// a = 49243763485
+
+// #include<iostream>
+// using namespace std;
+
+// int a = 90;
+
+// int main()
+// {
+//     int a = 10;
+//     {
+//         int a = 20;
+//         {
+//             int a = 30;
+//             {
+//                 int a = 50;
+//                 cout<<a<<endl;
+//             }
+//             cout<<a<<endl;
+//         }
+//         cout<<a<<endl;
+//     }
+//     cout<<a<<endl;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// const int a = 90;
+// int main()
+// {
+//     // int a = 10;
+//     {
+//         a = 90;
+//         cout<<a<<endl;
+//     }
+// }
+
+
+// =============================
+// DataType
+// int - 23,54,65,3,-23,54,64
+// float - 2.453245
+// char - '3', 'a', 'A', '*', ' '
+// double - 4.23454253456
+// string - "dasd","asdg"
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 90;
+//     float b = 9.4;
+//     char c = 'f';
+//     double d = 93.52352;
+//     string e = "aadesh";
+
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+//     cout<<c<<endl;
+//     cout<<d<<endl;
+//     cout<<e<<endl;
+// }
+
+
+// ================================
+// identifiers
+// variable, class, function, array, struct, union, objects, label 
+// 1) 2rollnumber, 5star, 23abc this are not allowed
+// 2) roll_number, _names, name_, first_name, my_age
+// 3) letters numbers and underscore : 
+// roll_number21, star5, abc_32
+// 4) case sensetive
+// a = 10; 
+// A = 20; 
+// 5) we cant user keywords as a identifier
+
