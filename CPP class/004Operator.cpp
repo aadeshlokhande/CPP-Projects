fcb65ebd38e7f4281952db9724c10b740ba93403
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-06 17:27:50 +0530

    06-09-2022

---
+++ CPP class/004Operator.cpp
@@ -0,0 +1,53 @@
+// arithmatic operator
+    // unary
+        // (+,-, ++, --)
+
+            // #include<iostream>
+            // using namespace std;
+            // int main()
+            // {
+            //     int a = 10,b;
+                // cout<<+a<<endl;
+                // cout<<-a<<endl;
+                // ++a;
+                // cout<< a <<endl;
+                // --a;
+                // cout<< a <<endl;
+                
+                // pre 
+                // b = ++a;
+                // cout<<"a = "<<a<<" and b = "<<b<<endl;
+
+                // post
+                // b = a++;
+                // cout<<"a = "<<a<<" and b = "<<b<<endl;
+            // }
+
+    // binary
+    // #include<iostream>
+    // using namespace std;
+    // int main()
+    // {
+    //     int a = 10, b = 20;
+    //     cout<< a + b << endl;
+    //     cout<< a - b << endl;
+    //     cout<< a * b << endl;
+    //     cout<< a / b << endl;
+    //     cout<< a % b << endl;
+    // }
+
+// +++++++++++++++++++Homework+++++++++++++++++++++++
+// get 2 values from user and print addition
+// get 2 values from user and print sub
+// get 2 values from user and print multi
+// get 2 values from user and print div
+// get 2 values from user and print mod 
+
+// get radious and print area of circle 
+// get height and base and print area of triangle
+
+
+
+// relationship operator
+// logical operator
+// assignmant operator 
