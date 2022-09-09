f65d73181f44daa1b720b8cc659ce0e94a5feccb
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-09 19:05:55 +0530

    09-09-2022

---
+++ CPP class/005ConditionalStatements.cpp
@@ -0,0 +1,153 @@
+// conditional statements
+// if
+// if(condition)
+// {
+//     statements
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout<<"enter your age = ";
+//     cin>>age;
+//     if(age>18)
+//     {
+//         cout<<"Hello"<<endl;
+//     }
+// }
+
+
+// if else 
+// if(condition)
+// {
+//     statements;
+// }
+// else 
+// {
+//     statements;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num; 
+//     cout<<"enter a number = ";
+//     cin>>num;
+//     if(num>0)
+//     {
+//         cout<<"+ve number"<<endl;
+//     }
+//     else
+//     {
+//         cout<<"-ve number"<<endl;
+//     }
+// }
+
+
+// if else ladder 
+
+// if(condition)
+// {
+//     statements;
+// }
+// else if(condition)
+// {
+//     statements;
+// }
+// else if(condition)
+// {
+//     statements;
+// }
+// .
+// .
+// .
+// else
+// {
+//     statements;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num; 
+//     cout<<"enter a number = ";
+//     cin>>num;
+//     if(num>0)
+//     {
+//         cout<<"+ve number"<<endl;
+//     }
+//     else if(num<0)
+//     {
+//         cout<<"-ve number"<<endl;
+//     }
+//     else 
+//     {
+//         cout<<"zero"<<endl;
+//     }
+// }
+
+
+
+// nested if else 
+// if(condition)
+// {
+//     statements;
+//     if(condition)
+//     {
+//         statements;
+//     }
+//     else 
+//     {
+//         statements;
+//     }
+// }
+// else 
+// {
+//     statements;
+//     if(condition)
+//     {
+//         statements;
+//     }
+//     else 
+//     {
+//         statements;
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout<<"Enter your age = ";
+//     cin>>age;
+//     if(age>0 && age < 80)
+//     {
+//         if(age>18)
+//         {
+//             cout<<"you can drive"<<endl;
+//         }
+//         else 
+//         {
+//             cout<<"you can't drive"<<endl;
+//         }
+//     }
+//     else
+//     {
+//         cout<<"invalid age"<<endl;
+//     }
+// }
+
+
+// +++++++++++ Homework ++++++++++++
+// get 3 angles from user and check its triangle or not 
+// get number and check its even or odd 
+// get a year and check its leap year or not 
+
+// get 3 numbers and print greatest number
