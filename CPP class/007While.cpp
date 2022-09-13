ca23220cb1e17e5f27ea1bbb64855d4517fbb60c
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-13 18:52:41 +0530

    13-09-2022

---
+++ CPP class/007While.cpp
@@ -0,0 +1,62 @@
+// while loop 
+
+// while(condition)
+// {
+//     statements;
+// }
+
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+//     int a = 1;
+//     while(a<=10)
+//     {
+//         cout<<a<<endl;
+//         ++a;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+//     int a1,a2,a = 1;
+//     while(a!=0)
+//     {
+//         cout<<"Enter a number = ";
+//         cin>>a1;
+//         cout<<"Enter a number = ";
+//         cin>>a2;
+//         a = a1+a2;
+//         cout<<a1<<"+"<<a2<<"="<<a<<endl;
+//     }
+// }
+
+
+
+// Do while 
+// do
+// {
+//     statements;
+// }while(condition);
+
+
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+//     int a1,a2,a;
+//     do
+//     {
+//         cout<<"Enter a number = ";
+//         cin>>a1;
+//         cout<<"Enter a number = ";
+//         cin>>a2;
+//         a = a1+a2;
+//         cout<<a1<<"+"<<a2<<"="<<a<<endl;
+//     }while(a!=0);
+// }
+
+
