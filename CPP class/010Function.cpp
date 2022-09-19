faf43ea2eebb9ba8f857b11c791477531d4f0920
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-19 18:29:54 +0530

    19-09-2022

---
+++ CPP class/010Function.cpp
@@ -0,0 +1,110 @@
+// Function
+
+// returntype functionName(arguemnts)
+// {
+//     statements;
+//     return value;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int cube(int a)
+// {
+//     int ans;
+//     ans = a *a*a;
+//     return ans;
+// }
+
+
+// int main()
+// {
+//     int var1 = 4,cb;
+//     cb = cube(var1);
+//     cout<<cb<<endl;
+//     cout<<cube(2)<<endl;
+//     cout<<cube(3)<<endl;
+//     cout<<cube(8)<<endl;
+//     cout<<cube(15)<<endl;
+// }
+
+
+
+// ====================================
+
+// types of function 
+
+// with argument with return 
+// #include<iostream>
+// using namespace std;
+// int cube(int a)
+// {
+//     int ans;
+//     ans = a *a*a;
+//     return ans;
+// }
+
+// int main()
+// {
+//     int a;
+//     a = cube(6);
+//     cout<<a<<endl;
+// }
+
+
+// with argument without return 
+// #include<iostream>
+// using namespace std;
+// void cube(int a)
+// {
+//     int ans;
+//     ans = a *a*a;
+//     cout<<ans<<endl;
+// }
+
+// int main()
+// {
+//     cube(8);
+// }
+
+
+
+// without argument with return 
+// #include<iostream>
+// using namespace std;
+// int cube()
+// {
+//     int ans;
+//     int a = 7;
+//     ans = a *a*a;
+//     return ans;
+// }
+
+// int main()
+// {
+//     cout<<cube()<<endl;
+// }
+
+// without argument without return 
+// #include<iostream>
+// using namespace std;
+// void cube()
+// {
+//     int ans;
+//     int a = 5;
+//     ans = a *a*a;
+//     cout<<ans<<endl;
+// }
+
+// int main()
+// {
+//     cube();
+// }
+
+// ++++++++++++++ HomeWork ++++++++++++
+// area of circle 
+// area of square 
+// area of triangle 
+// area of rectangle
+// area of cube 
+// area of cylinder 
