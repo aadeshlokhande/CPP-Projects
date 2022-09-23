d0ff7c9f7b405786475a2f4d989dd4bddc8b117d
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-23 18:31:03 +0530

    23-09-2022

---
+++ CPP class/013array.cpp
@@ -0,0 +1,28 @@
+// Array 
+
+// Datatype arrayName[size];
+// int abc[5];
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+    // int abc[5] = {23,54,76,87,23};
+    // abc[0] = 10;
+    // cout<<abc[0]<<endl;
+    // cout<<"Enter a number = ";
+    // cin>>abc[3];
+    // cout<<abc[3]<<endl;
+
+    // for(int a = 0; a<5; a++)
+    // {
+    //     cout<<"Enter a number = ";
+    //     cin>>abc[a];
+    // }
+
+    // for(int a = 0; a<5; a++)
+    // {
+    //     cout<<abc[a]<<endl;
+        
+    // }
+// }
---
+++ CPP class/014MultidimentionalArray.cpp
@@ -0,0 +1,91 @@
+// multi-dimentional Array 
+
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+    // int a[3][5] = {
+    //     {12,43,54,65,76},
+    //     {23,54,25,64,65},
+    //     {34,65,34,65,35}
+    //     };
+    
+    // cout<<a[1][3]<<endl;
+    // for(int i= 0; i<3; i++)
+    // {
+    //     for(int j=0; j<5; j++)
+    //     {
+    //         cout<<a[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i= 0; i<3; i++)
+    // {
+    //     for(int j=0; j<5; j++)
+    //     {
+    //         cout<<"a["<<i<<"]["<<j<<"] = "<<a[i][j]<<endl;
+    //     }
+    // }
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a[3][4];
+//     for(int i= 0; i<3; i++)
+//     {
+//         for(int j=0; j<4; j++)
+//         {
+//             cout<<"enter a number = ";
+//             cin>>a[i][j];
+//         }
+//     }
+
+//     for(int i= 0; i<3; i++)
+//     {
+//         for(int j=0; j<4; j++)
+//         {
+//             cout<<"a["<<i<<"]["<<j<<"] = "<<a[i][j]<<endl;
+//         }
+//     }
+// }
+
+
+
+
+// ------------------------------------
+// mark[20][6] = {};
+
+// press 1 marks
+//     enter a rollnumber = 3
+//     press 1 : hindi 
+//     press 2 : marathi 
+//     .
+//     .
+//     .
+//     enter your choice = 2
+
+//         marathi = 84
+
+
+// press 2 result
+//     enter a roll number = 14
+//     hindi = 54
+//     marathi = 45 
+//     .
+//     .
+//     .
+
+// press 3 whole result
+//                         hin     mar     english......
+//     roll number 0 
+//     roll number 1 
+//     roll number 2 
+//     roll number 3 
+//     roll number 4 
+//     roll number 5 
+//     roll number 6 
