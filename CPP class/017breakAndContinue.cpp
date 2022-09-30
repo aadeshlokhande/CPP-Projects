614e1fe136bd68032f3c7c84a5df6f5bcf46eb73
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-30 18:43:05 +0530

    30-09-2022

---
+++ CPP class/017breakAndContinue.cpp
@@ -0,0 +1,21 @@
+// break and continue
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i = 1; i<=10; i++)
+    {
+        if(i==5)
+        {
+            // break;
+            continue;
+            // cout<<"Pragati\n";
+        }
+        else 
+        {
+            cout<<i<<endl;
+        }
+    }
+    cout<<"HEllo\n";
+}
