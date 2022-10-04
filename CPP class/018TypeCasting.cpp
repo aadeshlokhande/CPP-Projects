df35f67a665b76b9f249dcb2095133dad0d8527b
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-10-04 18:27:23 +0530

    04-10-2022

---
+++ CPP class/018TypeCasting.cpp
@@ -0,0 +1,47 @@
+// Type Conversion 
+// implicit type conversion
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 10;
+//     float b = a;
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float b = 12.34;
+//     int a = b;
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+// }
+
+
+// explicit type conversion - type casting 
+// c - type casting 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float a = 10.34;
+//     int b = (int)a;
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+// }
+
+
+// functional type casting 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float a = 10.34;
+//     int b = int(a);
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+// }
