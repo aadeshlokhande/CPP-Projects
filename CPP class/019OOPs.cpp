1222eb741090126dc89b74ebd998474da063adea
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-10-07 18:36:14 +0530

    07-10-2022

---
+++ CPP class/019OOPs.cpp
@@ -0,0 +1,26 @@
+// OOPs - Object Oriented Programming 
+
+// class  - 
+// object - 
+        // constructor
+        // distructor
+        // access modifiers - private , public
+        // friend function
+
+// inheritance - 
+        // single inheritance
+        // multiple inheritance
+        // multilevel inheritance
+        // heirarchical inheritance
+        // hybrid
+        // acess modifier - protected
+
+// polymorphism - many forms
+        // function overloading
+        // function overiding
+
+// abstraction 
+// encapsulation
+
+// file handling
+// error handling 
