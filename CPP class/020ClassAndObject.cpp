36e77551cb829d2135b2e70b9ca69069f07935d5
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2022-10-15 08:54:10 +0530

    15-10-2022

---
+++ CPP class/020ClassAndObject.cpp
@@ -0,0 +1,69 @@
+// #include<iostream>
+// using namespace std;
+
+// class Animal
+// {
+//     public:
+//     int Leg, Eye, Horn;
+//     string Name;
+// };
+
+// int main()
+// {
+//     Animal monkey;
+//     monkey.Leg = 2;
+//     monkey.Eye = 2;
+//     monkey.Horn = 0;
+//     monkey.Name ="jumbo" ;
+    
+//     cout<<monkey.Name<<endl;
+//     cout<<monkey.Leg<<endl;
+//     cout<<monkey.Eye<<endl;
+//     cout<<monkey.Horn<<endl;
+// }
+
+
+
+// ------------------------------------------
+// #include<iostream>
+// using namespace std;
+// class Animal
+// {
+//     public:
+//     int Leg, Eye, Horn;
+//     string Name;
+    
+//     void getData(string name,int leg,int eye,int horn)
+//     {
+//         Name = name;
+//         Leg = leg;
+//         Eye = eye;
+//         Horn = horn;
+        
+//     }
+    
+//     void printData()
+//     {
+//         cout<<"Name = "<<Name<<endl;
+//         cout<<"Leg = "<<Leg<<endl;
+//         cout<<"Eye = "<<Eye<<endl;
+//         cout<<"Horn  "<<Horn<<endl;
+//     }
+// };
+
+// int main()
+// {
+//     Animal monkey,tiger,zebra;
+//     monkey.getData("acha", 2,2,0);
+//     tiger.getData("sheru", 4,2,0);
+//     zebra.getData("pandu", 4,2,0);
+    
+//     cout<<"monkey\n";
+//     monkey.printData();
+//     cout<<"tiger\n";
+//     tiger.printData();
+//     cout<<"zebra\n";
+//     zebra.printData();
+// }
+
+// ==========================
